10818a4ec081174ee8d97832a0dc096ed9cb4018
TinTooth <boconne4@gmail.com>
2022-11-02 11:52:24 -0500

    Added filter functions to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import axios  from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import MusicTable from './Components/MusicTable/MusicTable';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
@@ -11,18 +12,23 @@ function App() {
     getAllSongs();
   },[]);
 
-
   async function getAllSongs() {
     const response = await axios.get('http://127.0.0.1:8000/api/music/');
     setSongs(response.data);
   }
 
-  const displayArray = () => {console.log(songs)} 
+  function filter(field){
+    const filteredSongs = [...songs].sort((a,b)=> (a[field] > b[field]) ? 1 : b[field] > a[field] ? -1 : 0)
+    setSongs(filteredSongs);
+  }
+  function filterString(field){
+    const filteredSongs = [...songs].sort((a,b)=> (a[field].toLowerCase() > b[field].toLowerCase()) ? 1 : b[field].toLowerCase() > a[field].toLowerCase() ? -1 : 0)
+    setSongs(filteredSongs);
+  }
 
-  return (
+  return (  
     <div>
-      <MusicTable songs={songs}/>
-      <div onClick={displayArray}>CLICK</div>
+      <MusicTable songs={songs} filter = {filter} filterString = {filterString}/>
     </div>
   );
 }
--- src/Components/MusicTable/MusicTable.jsx
+++ src/Components/MusicTable/MusicTable.jsx
@@ -1,27 +1,39 @@
 import React from "react";
 
-const MusicTable = ({songs}) => {
+const MusicTable = ({songs, filter, filterString}) => {
+
+    const handleSongClick = () => { filter('id')};
+    const handleTitleClick = () => { filterString('title')};
+    const handleArtistClick = () => { filterString('artist')};
+    const handleAblumClick = () => { filterString('album')};
+    const handleGenreClick = () => { filterString('genre')};
+    const handleReleaseDateClick = () => { filter('release_date')};
+    const handleLikesClick = () => { filter('likes')};
+
     return (  
         <table>
             <thead>
                 <tr>
-                    <th> Song Title</th>
-                    <th> Artist</th>
-                    <th> Album</th>
-                    <th> Genre</th>
-                    <th> Release Date</th>
-                    <th> Total Likes</th>
+                    <th onClick={handleSongClick}> Song Number</th>
+                    <th onClick={handleTitleClick}> Song Title</th>
+                    <th onClick ={handleArtistClick}> Artist</th>
+                    <th onClick = {handleAblumClick}> Album</th>
+                    <th onClick = {handleGenreClick}> Genre</th>
+                    <th onClick = {handleReleaseDateClick}> Release Date</th>
+                    <th onClick = {handleLikesClick}> Total Likes</th>
                 </tr>
             </thead>
             <tbody>
+    
                 {songs.map((song,i) => {
                     return(
                         <tr key = {i}>
+                            <td>{song.id}</td>
                             <td>{song.title}</td>
                             <td>{song.artist}</td>
                             <td>{song.album}</td>
-                            <td>{song.release_date}</td>
                             <td>{song.genre}</td>
+                            <td>{song.release_date}</td>
                             <td>{song.likes}</td>
                         </tr>
                     )
